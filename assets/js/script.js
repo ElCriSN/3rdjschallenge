bbfd2f4e085cd9de27f2313267e0a0b4b249bc00
ElCriSN <cristian.faundezp@utem.cl>
2022-07-21 10:32:13 -0400

    :))!!
    :)!!!!

---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+//SCOPE EN JAVASCRIPTT JAJAJAJA =D!!
+HAY TRES TIPOS DE SCOPES = GLOBAL, FUNCIÓN, BLOQUE =)!! JAJAJA =P!
+
+/* AQUÍ ESTAMOS EN UN SCOPE GLOBAL */
+
+if (a == 5) {
+    /* AQUÍ ESTAMOS EN UN SCOPE DE BLOQUE  JAJAJAA =D!!*/
+}
+
+// ARROW FUNCTIONS Y ADDEVENTLISTERN =DD!! AJAJAJA =P !! =)!!
+
+// btn = document.querySelector("button")
+// btn.addEventListener("click", () => alert('hola'))
+
+// ARROW FUNCTIONS =DDD JAJAJA =)!!! =P!!
+// suma = (a, b) => a + b
+
+//Es lo mismo que =)!!:
+
+// suma = function (a, b) {
+//     return a + b
+// }
+
+// SUMA CON MÚLTIPLES LÍNEAS EN ARROW FUNCTION =P!!!
+
+// suma = (a, b) => {
+//     console.log(a)
+//     console.log(b)
+//     return a + b
+// }
+
+// CUANDO ES UNA SOLA LÍNEA EL RETURN ES IMPLÍCITOOO JAJAAJA =P!! =DDD!! =)!!
+
+// suma = (a, b) => a + b /* a + b es lo mismo que return a + b */
+
+// () alert('hola')
+
+// function alertar(){
+//     alert("hola")
+// }// alertar = function() {
+//     alert("hola");
+// }
+
+// alertar()
+
+// function ejemplo(par-1){
+//     par-1() /* AQUÍ LLAMAMOS A LA FUNCIÓN PASADA */
+// }
+
+// ejemplo(function() {alert ("hola")} )
+    
+/* script.js */
+// function alertar(){
+//     alert("hola =DDD!! jaajajajjaa");
+// }
+
+// btn = document.querySelector("button");
+// btn.addEventListener("click", alertar);
+
+// function pintar(color) {
+//     elemento = document.querySelector("body");
+//     elemento.style.backgroundColor = color;
+// }
+
+// btn = document.querySelector("button")
+// btn.addEventListener("click", pintar("black"))
+
+// btn = document.querySelector("button")
+
+/* Ejemplo de uso con Función Normal ajjajaa =)!! */
+// function alertar() {
+//     alert("hola jajajaa =DDD!! =PPP!!");
+// }
+
+// btn.addEventListener("click", alertar)
+
+/* Ejemplo de uso con Función Anónima */
+// btn.addEventListener("click", function() {
+//     alert("hola")
+// });
