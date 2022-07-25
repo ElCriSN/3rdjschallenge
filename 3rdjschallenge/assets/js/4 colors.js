ce4e865610d4a64f60ad53a478874a53721d085a
ElCriSN <cristian.faundezp@utem.cl>
2022-07-25 08:30:12 -0400

    :)!!
    :)!!!

--- 3rdjschallenge/assets/js/4 colors.js
+++ 3rdjschallenge/assets/js/4 colors.js
@@ -1,14 +1,31 @@
-green = document.querySelector("#green");
-blue = document.querySelector("#blue");
-brown = document.querySelector("#brown");
-violet = document.querySelector("#violet");
-all = document.querySelector(".container");
-all.addEventListener('keydown', function (event) {
+const colorDivs = document.addEventListener('keydown', function (event) {
+    green = document.querySelector("#green");
+    blue = document.querySelector("#blue");
+    brown = document.querySelector("#brown");
+    violet = document.querySelector("#violet");
+
     if (event.key === 'a') {
-        goodGuysColors.style.backgroundColor = "blue";
+        green.style.backgroundColor = "brown";
     } else if (event.key === 's') {
-        /* Cambiar a color 2 */
-    } else (event.key === 'd') {
-        /* Cambiar a color 3 */
+        blue.style.backgroundColor = "violet";
+    } else if (event.key === 'd') {
+        brown.style.backgroundColor = "green";
+    } else {
+        violet.style.backgroundColor = "blue";
     }
-})
+
+    green.addEventListener("click", function(){
+        green.style.backgroundColor = "green";
+    })
+    blue.addEventListener("click", function(){
+        blue.style.backgroundColor = "blue";
+    })
+    brown.addEventListener("click", function(){
+        brown.style.backgroundColor = "brown";
+    })
+    violet.addEventListener("click", function(){
+        violet.style.backgroundColor = "violet";
+    })
+
+
+});
--- 3rdjschallenge/assets/js/script.js
+++ 3rdjschallenge/assets/js/script.js
@@ -1,15 +1,17 @@
-// FUNCIÓN NORMAL EJERCICIO 3 DESAFÍO 3 =P!!
 
-// function pintar(){
-//     ele = document.getElementById("ele1");
+
+// ele = document.getElementById("ele1");
+
+// FUNCIÓN ANÓNIMA DEL CÓDIGO ANTERIOOR JAJAJJAA =DDD! =P!! =)!! PARTE 3.1 DESAFÍO 3 =P!!
+
+// ele.addEventListener("click", function(){
+    
 //     ele.style.backgroundColor = "yellow";
-// }
+// });
 
-ele = document.getElementById("ele1");
-// ele.addEventListener("click", pintar);
+// FUNCIÓN DECLARA CON PARAMETRO POR DEFECTO GREEN EJERCICIO 3 DESAFÍO 3 PARTE 3.2 =P!!
 
-// FUNCIÓN ANÓNIMA DEL CÓDIGO ANTERIOOR JAJAJJAA =DDD! =P!! =)!!
-
-ele.addEventListener("click", function(color = "green"){
+function pintar(color="green"){
+    ele = document.getElementById("ele1");
     ele.style.backgroundColor = color;
-});
+}
